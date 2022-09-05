75d40c10b7d753dd9dde5dc77f117bfebc6c16b7
Antonius131 <antomendetta@gmail.com>
2022-09-05 15:36:22 +0200

    creating Product __construct function

--- parent-classes/product.php
+++ parent-classes/product.php
@@ -5,4 +5,12 @@
       protected $price;
       protected $image;
       protected $description;
+
+      function __construct($_name, $_price, $_image, $_description)
+      {
+         $this->name = $_name;
+         $this->price = $_price;
+         $this->image = $_image;
+         $this->description = $_description;
+      }
    }
