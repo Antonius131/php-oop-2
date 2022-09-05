bd1563211375a350c736b344d3a06f9325a3c2d8
Antonius131 <antomendetta@gmail.com>
2022-09-05 16:13:42 +0200

    creating Food construct with his parent construct

--- child-classes/products/food.php
+++ child-classes/products/food.php
@@ -1,9 +1,17 @@
 <?php
 
-   require_once __DIR__ . '/../../parent-classes/product.php';
+   require __DIR__ . '/../../parent-classes/product.php';
 
    class Food extends Product {
       protected $expirationDate;
       protected $weight;
       protected $origin;
+
+      function __construct($_name, $_price, $_image, $_description, $_expirationDate, $_weight, $_origin)
+      {
+         parent::__construct($_name, $_price, $_image, $_description);
+         $this->expirationDate = $_expirationDate;
+         $this->weight = $_weight;
+         $this->origin = $_origin; 
+      }
    }
