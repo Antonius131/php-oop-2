6458ebbdffd9b03706c09318d106f962d98735c4
Antonius131 <antomendetta@gmail.com>
2022-09-05 17:30:20 +0200

    creating Bed contruct function with his parent

--- models/products/bed.php
+++ models/products/bed.php
@@ -4,4 +4,12 @@ class Bed extends Product {
    private $length;
    private $height;
    private $depth;
+
+   public function __construct($_name, $_price, $_image, $_description, $_length, $_height, $_depth)
+   {
+      parent::__construct($_name, $_price, $_image, $_description);
+      $this->length = $_length;
+      $this->height = $_height;
+      $this->depth = $_depth; 
+   }
 }
