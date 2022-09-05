5ab873b9c599243d442f9da67a11c03fbec21358
Antonius131 <antomendetta@gmail.com>
2022-09-05 17:17:46 +0200

    creating Toy construct function with his parent

--- models/products/toy.php
+++ models/products/toy.php
@@ -4,4 +4,12 @@
       private $type;
       private $color;
       private $size;
+
+      public function __construct($_name, $_price, $_image, $_description, $_type, $_color, $_size)
+      {
+         parent::__construct($_name, $_price, $_image, $_description);
+         $this->type = $_type;
+         $this->color = $_color;
+         $this->size = $_size; 
+      }
    }
