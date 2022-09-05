eb6303d37cbbe6db99718c6648e89e0f2fddd6a8
Antonius131 <antomendetta@gmail.com>
2022-09-05 17:57:34 +0200

    includine User model in index.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 
    include_once './models/product.php' ;
+   include_once './models/user.php';
    include_once './models/products/food.php';
    include_once './models/products/toy.php';
    include_once './models/products/bed.php';
+
+   // $cuccetta = new Bed('nome', 220, 'img::http', 'descrizione', '1m', '1m', '1m');
+   // var_dump($cuccetta);
